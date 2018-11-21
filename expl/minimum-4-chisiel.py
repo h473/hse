cbfee6459a330db88de926fbc24bdd83e5a09635
h473 <chetotrov@gmail.com>
2018-11-21 11:16:46 +0300

    done

--- expl/minimum-4-chisiel.py
+++ expl/minimum-4-chisiel.py
@@ -1,12 +1,12 @@
 a = int(input())
 b = int(input())
 c = int(input())
-d = int(input())
+f = int(input())
 
 
-def minimal(a, b, c, d):
-    result = min(min(a, b), min(c, d))
+def minimal(a, b, c, f):
+    result = min(min(a, b), min(c, f))
     return result
 
 
-print(minimal(a, b, c, d))
+print(minimal(a, b, c, f))
