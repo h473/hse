19907fbc47527ba37a09f932c8ef009f6e1206fd
h473 <chetotrov@gmail.com>
2018-11-27 16:54:00 +0300

    done

---
+++ expl/flaghi.py
@@ -0,0 +1,12 @@
+n = int(input())
+for i in range(n):
+    print("+___ ", end="", sep="")
+print()
+for i in range(n):
+    print("|", i + 1, " / ", end="", sep="")
+print()
+for i in range(n):
+    print("|__\ ", end="", sep="")
+print()
+for i in range(n):
+    print("|    ", end="", sep="")
